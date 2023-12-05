8a408a184cd55f8d917c7c27ffd5af2ee6a5b13c
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-12-05 13:03:59 +0700

    add mabac

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import axios from 'axios';
 import { getGpu, getGpuYear } from './database.js';
+import cors from 'cors';
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
